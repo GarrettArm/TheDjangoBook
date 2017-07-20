07c4f535848799ac0f10abbb7e33689104c74c76
garrett armstrong <garrett.w.armstrong@gmail.com>
2017-07-19 23:49:49 -0500

    annotated the render shortcut and what its magic pieces are

--- mysite_project/books/views.py
+++ mysite_project/books/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from books.models import Book
+# from django.template.loader import get_template
+# from django.http import HttpResponse, Http404
 
 
 def search_form(request):
@@ -12,9 +14,22 @@ def search(request):
     except KeyError:
         return render(request, 'books/search_form.html', {'error': False})
 
+    # because request.GET will give an empty string, if the passed in variable is empty
     if not search_string:
         return render(request, 'books/search_form.html', {'error': True})
 
-    books = Book.objects.filter(title__icontains=search_string)
+    # verbose step-by-step of the magic render shortcut
+    books_queryset = Book.objects.filter(title__icontains=search_string)
+    # template_file = 'books/search_results.html'
+    # template_text = get_template(template_file)
+    # context = {'books': books_queryset, 'query': search_string}
+    # SafeText = template_text.render(context)
+    # return HttpResponse(SafeText)
+
+    # more and more succinct but more magic ways of saying this
+    # t = get_template('books/search_results.html')
+    # return render(request, t, context)
+
+    # most magic & succinct way
     return render(request, 'books/search_results.html',
-                  {'books': books, 'query': search_string})
+                  {'books': books_queryset, 'query': search_string})
