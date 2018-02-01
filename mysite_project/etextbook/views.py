10553aa3dfe35bf925cfc7c544b14f2a2de43f2c
GarrettArm <garrett.w.armstrong@gmail.com>
2018-01-31 23:54:49 -0600

    removed commented out code

--- mysite_project/etextbook/views.py
+++ mysite_project/etextbook/views.py
@@ -33,23 +33,3 @@ def return_spreadsheet(request, filepath=None):
         response = HttpResponse(f, content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
         return response
-
-# def read_spreadsheet(request):
-#     if request.method == 'POST':
-#         form = UploadFileForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             filename = request.FILES['document'].name
-#             if os.path.split(filename)[1] != '.csv':
-#                 form = UploadFileForm()
-#                 return render(request, 'etextbook/spreadsheet_page.html', {'form': form, 'errors': ['Uploaded file must be a csv.']})
-#             orig_csv = os.path.join('uploaded_spreadsheets', filename)
-#             new_csv = os.path.join('uploaded_spreadsheets', "cleaned_{}".format(filename))
-#             parse_bookstore_csv.main(orig_csv, new_csv)
-#             form.save()
-#             return return_spreadsheet(request, filepath=new_csv)
-#     else:
-#         form = UploadFileForm()
-#     return render(request, 'etextbook/spreadsheet_page.html', {'form': form})
-
-
-
--- mysite_project/polls/views.py
+++ mysite_project/polls/views.py
@@ -1,6 +1,4 @@
 from django.http import HttpResponseRedirect
-# from django.http import Http404
-# from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
