d48978448b8b6cbc48f036747a09ceac9f5e4736
GarrettArm <garrett.w.armstrong@gmail.com>
2018-02-24 14:40:01 -0600

    manage.py collectstatic pulls updated files from /static/ to nginx expected location

--- mysite_project/mysite/settings.py
+++ mysite_project/mysite/settings.py
@@ -144,6 +144,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = '/var/www/django/static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 INTERNAL_IPS = ['127.0.0.1', ]
