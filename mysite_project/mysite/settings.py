abaeab49eab5940f119bf805d45b9db82020c2f5
GarrettArm <garrett.w.armstrong@gmail.com>
2018-02-24 16:01:56 -0600

    sets DEBUG=False for real this time

--- mysite_project/mysite/settings.py
+++ mysite_project/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '386$!xv=xbqfw42#!#a*1zq+wo5^u(_v=y_%-myi&(yb)06pd_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
 
 
 # Application definition
