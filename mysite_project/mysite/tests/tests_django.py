a51b2627b3a5c9c594c12f1b361976e1593e3a4c
GarrettArm <garrett.w.armstrong@gmail.com>
2018-03-25 21:29:42 -0500

    moving toward djangos client test library

--- mysite_project/mysite/tests/tests_django.py
+++ mysite_project/mysite/tests/tests_django.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 import unittest
-from django.test import Client
+from django.test import TestCase
 
 
-class LoginWorks(unittest.TestCase):
-    def setUp(self):
-        self.client = Client()
+class LoginWorks(TestCase):
 
     def test_get_root_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_template_loads(self):
+        response = self.client.get('/')
+        template_names = [i.name for i in response.templates]
+        self.assertIn('mysite/frontpage.html', template_names)
+        self.assertIn('base.html', template_names)
+        self.assertTemplateUsed(response, 'base.html')
 
     def test_post_login(self):
         response = self.client.post('/accounts/login/', {'admin': 'admin', })
