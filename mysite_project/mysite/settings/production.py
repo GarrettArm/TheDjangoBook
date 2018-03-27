195ec7182f89f929c2846d4715437525012195e2
GarrettArm <garrett.w.armstrong@gmail.com>
2018-03-26 21:50:23 -0500

    hardcoded allowed_hosts only to my aws elastic ip

--- mysite_project/mysite/settings/production.py
+++ mysite_project/mysite/settings/production.py
@@ -5,7 +5,7 @@ with open('../../django_secret_key.txt', 'r') as f:
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['18.220.175.184', ]
+ALLOWED_HOSTS = ['18.188.30.182', ]
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
