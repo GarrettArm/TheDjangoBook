99c66aabb7896453b93fb29ca5146d27d0482cd4
GarrettArm <garrett.w.armstrong@gmail.com>
2018-05-15 11:08:35 -0500

    no longer ignores file if it is a wrong mimetype

--- mysite_project/etextbook/forms.py
+++ mysite_project/etextbook/forms.py
@@ -7,9 +7,3 @@ class UploadFileForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = ('description', 'document')
-
-    def clean_document(self):
-        data = self.cleaned_data['document']
-        if data.content_type != 'text/csv':
-            raise forms.ValidationError('The file must be a csv.')
-        return data
