e8da4c2788121986e8ef3b2f08cae924d54e5d4a
GarrettArm <garrett.w.armstrong@gmail.com>
2019-01-17 11:32:43 -0600

    updated text

--- mysite_project/chat/consumers.py
+++ mysite_project/chat/consumers.py
@@ -7,7 +7,7 @@ import json
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = 'chat_{}'.format(self.room_name)
 
         # Join room group
         await self.channel_layer.group_add(
--- mysite_project/site_core/settings/runserver.py
+++ mysite_project/site_core/settings/runserver.py
@@ -22,6 +22,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'runserver_db',
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
