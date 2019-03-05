796bfcb73956dafd3cf9b9743377855420b04340
GarrettArm <garrett.w.armstrong@gmail.com>
2019-03-05 17:04:56 -0600

    drf update requires new argument passed

--- mysite_project/vue_test/routers.py
+++ mysite_project/vue_test/routers.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from .views import FuelEffeciencyViewSet
 
 router = routers.DefaultRouter()
-router.register('vue_api', FuelEffeciencyViewSet)
+router.register('vue_api', FuelEffeciencyViewSet, base_name='FuelEffeciency')
