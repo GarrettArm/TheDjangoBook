0ac47572be7209ad725e8a784d4bcc4b89e8bd47
GarrettArm <garrett.w.armstrong@gmail.com>
2019-03-05 21:48:02 -0600

    whitespace change

--- wipe_volumes.sh
+++ wipe_volumes.sh
@@ -45,13 +45,7 @@ docker image ls
 docker ps -a
 
 docker-compose up -d --build
-docker-compose run webapp python3 manage.py makemigrations vue_test polls etextbook contact ajax_polls shwagswap --settings=site_core.settings.production
-# docker-compose run webapp python3 manage.py makemigrations  --settings=site_core.settings.production
-# docker-compose run webapp python3 manage.py makemigrations  --settings=site_core.settings.production
-# docker-compose run webapp python3 manage.py makemigrations  --settings=site_core.settings.production
-# docker-compose run webapp python3 manage.py makemigrations  --settings=site_core.settings.production
-# docker-compose run webapp python3 manage.py makemigrations  --settings=site_core.settings.production
-docker-compose run webapp python3 manage.py migrate --settings=site_core.settings.production
-# docker-compose run webapp python3 manage.py createsuperuser --settings=site_core.settings.production
-docker-compose run webapp python3 manage.py collectstatic --settings=site_core.settings.production
-
+docker-compose run webapp python3 manage.py makemigrations vue_test polls etextbook contact ajax_polls shwagswap --settings=site_core.settings.development
+docker-compose run webapp python3 manage.py migrate --settings=site_core.settings.development
+# docker-compose run webapp python3 manage.py createsuperuser --settings=site_core.settings.development
+docker-compose run webapp python3 manage.py collectstatic --settings=site_core.settings.development
